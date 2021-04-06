52321eda3ebe14c0723ba2950c226d474f94346e
Stephanie Yung <Stephanie.Yung43@myhunter.cuny.edu>
2021-04-05 22:37:19 -0400

    Added Template for WYR cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+// import {Container, Card} from 'react-bootstrap'
+import Card from './Card/Card.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        <u>
+          ...Would You Rather...
+        </u>
+      </h2>
+      <div class='row'>
+        <div class="column">
+          <Card 
+            title='Card Title'
+            imgURL='https://cdn.ndtv.com/tech/images/gadgets/thumbs/pikachu_hi_pokemon_thumb.jpg'
+            trait = 'happy'
+            food='popcorn'
+            song = 'Party in the USA'
+            hero='superman'
+          />
+        </div>
+        <div class="column">
+          <Card 
+            title='Card Title 2'
+            imgURL='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHuGltoqiUSh-zkD6KXnVTIBBsDNq6yR-AQ&usqp=CAU'
+            trait = 'spicy'
+            food='pasta'
+            song='Sorry not Sorry'
+            hero='batman'
+          />
+        </div>
+        
+      </div>      
+
     </div>
   );
 }
